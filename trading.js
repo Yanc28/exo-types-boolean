d4e4a90cbc8c886c0be9e211f0616bcd28b48420
Yanc28 <yanisbouabdellahw@gmail.com>
2021-02-17 10:14:49 +0100

    done

--- trading.js
+++ trading.js
@@ -13,3 +13,20 @@ else if (CurrentPrice <= 35000) {
 else {
   console.log("Hodl")
 }
+
+
+
+// correction
+
+let BTCprice = 50000 // price of BTC in US dollars
+let sellingprice = 60000
+let buyingprice = 45000
+
+if (BTCprice > sellingprice) {
+  (console.log('sell BTC'))
+} else if (BTCprice < buyingprice) {
+  console.log('buy BTC')
+} else {
+  console.log('HODL')
+}
+
